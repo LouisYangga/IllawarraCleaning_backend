b6a29b36417cff4de6d5179e9f33028eb3439922
LouisYangga <102441218+LouisYangga@users.noreply.github.com>
2025-06-09 15:41:57 +1000

    rabbit config in pricing

---
+++ pricing-service/src/main/java/com/example/pricing_service/config/RabbitMQConfig.java
@@ -0,0 +1,37 @@
+package com.example.pricing_service.config;
+
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.QueueBuilder;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitMQConfig {
+
+    @Value("${rabbitmq.queue.quotation.name}")
+    private String quotationQueueName;
+
+    @Bean
+    public Queue quotationQueue() {
+        return QueueBuilder
+                .durable(quotationQueueName)
+                .build();
+    }
+
+    @Bean
+    public MessageConverter jsonMessageConverter() {
+        return new Jackson2JsonMessageConverter();
+    }
+
+    @Bean
+    public RabbitTemplate rabbitTemplate(ConnectionFactory connectionFactory) {
+        RabbitTemplate template = new RabbitTemplate(connectionFactory);
+        template.setMessageConverter(jsonMessageConverter());
+        return template;
+    }
+}
