83a4714c045c67841f7ab76293688584cb080b07
LouisYangga <102441218+LouisYangga@users.noreply.github.com>
2025-06-26 12:30:30 +1000

    exclude HEAD for health checking with robot

--- pricing-service/src/main/java/com/example/pricing_service/config/JwtAuthFilter.java
+++ pricing-service/src/main/java/com/example/pricing_service/config/JwtAuthFilter.java
@@ -38,7 +38,7 @@ public class JwtAuthFilter extends OncePerRequestFilter {
                (path.startsWith("/api/prices/services/") && "GET".equals(method)) ||
                (path.equals("/api/prices/addons") && "GET".equals(method)) ||
                (path.startsWith("/api/prices/addons/") && "GET".equals(method)) ||
-               (path.equals("/api/prices/health") && "GET".equals(method));
+               (path.equals("/api/prices/health"));
     }
 
     @Override
